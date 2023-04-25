d789d9568bbb2d4155a8b9c8e1f54bcaf6911b49
Nitin Bhalala <bhalalanitin5291@gmail.com>
2023-04-25 13:30:19 +0530

    fix: text-size

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,20 +17,22 @@ function Card() {
         />
       </div>
       <div className="relative pt-7">
-        <div className="absolute flex flex-col items-center justify-center w-full -top-6">
+        <div className="absolute flex flex-col items-center justify-center w-full -top-4">
           <div className="inline-block p-2 bg-[#F4F0FF] rounded-full">
             <Image
               src="/images/user-1.jpg"
-              height="100"
-              width="100"
+              height="70"
+              width="70"
               alt="user-icon"
               className="text-white rounded-full hover:cursor-pointer"
             />
           </div>
-          <h2 className="text-xl font-bold capitalize">boris McCranie</h2>
+          <h2 className="text-lg pt-0.5 font-bold capitalize text-center">
+            boris McCranie
+          </h2>
         </div>
-        <div className="bg-white  rounded-[20px] shadow-2xl pt-20 pb-4">
-          <div className="flex  py-2 my-5 mx-3 bg-[#FAF8FF] rounded-[18px] justify-between">
+        <div className="bg-white  rounded-3xl shadow-2xl pt-[3.8rem] pb-4">
+          <div className="flex flex-wrap xs:flex-nowrap py-4 my-5 mx-3 bg-[#FAF8FF] rounded-[18px] justify-between">
             {selectedProileDetail.map((item, index) => {
               const { image, title, count } = item;
               return (
@@ -61,10 +63,11 @@ function Card() {
             })}
           </div>
 
-          <div className="px-8 mt-3">
+          <div className="px-4 xs:px-8 mt-3">
             <hr />
-            <div className="py-3 ">
-              <span className="capitalize px-2 border-l-[4px] font-semibold text-[#666469] border-[#FABA51]">
+            <div className="relative py-2 px-2">
+              <hr className="border-[0.12rem] font-semibold w-3.5  border-[#FABA51] absolute rotate-90 bottom-[19px] left-0" />
+              <span className="capitalize px-2  font-semibold text-[#666469] text-sm">
                 top ranking
               </span>
             </div>
@@ -73,14 +76,14 @@ function Card() {
               {UserData.map((user, index) => {
                 const { name, score, image } = user;
                 return (
-                  <div key={index} className="flex flex-col gap-2">
-                    <div className="text-[#523B90] flex items-center gap-2">
-                      <span className="block text-lg font-bold">{`#${
+                  <div key={index} className="flex  flex-col gap-2">
+                    <div className="text-[#523B90] flex flex-wrap xs:flex-nowrap justify-center xs:justify-start items-center gap-2">
+                      <span className="block min-w-[28px] text-base font-bold">{`#${
                         index + 1
                       }`}</span>
-                      <div className="inline-block p-1 bg-[#F4F0FF] rounded-full">
+                      <div className="flex p-1 bg-[#F4F0FF] rounded-full flex-shrink-0">
                         <Image
-                          src={`/images/${image ?? ""}.jpg`}
+                          src={`/images/${image ?? "user2"}.jpg`}
                           height="40"
                           width="40"
                           alt="user-icon"
@@ -92,7 +95,7 @@ function Card() {
                         <span className="block font-bold text-[#252427]">
                           {name ?? ""}
                         </span>
-                        <span className="block font-medium text-[#7E729A]">
+                        <span className="block font-medium text-sm text-[#7E729A] capitalize">
                           score:{" "}
                           <span className="text-[#523B90] font-bold">
                             {score ?? ""}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/Card";
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-6 bg-gradient-to-t from-[#4A2ABF] to-[#5632D9]`}
+      className={`flex min-h-screen flex-col items-center justify-between p-5 bg-gradient-to-t from-[#4A2ABF] to-[#5632D9]`}
     >
       <Card />
     </main>
